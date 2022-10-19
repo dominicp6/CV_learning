c39a9f8ae2bb7fe29068fea293b6c6f09d691ab7
dominicp6 <dom.phillips97@gmail.com>
2022-10-19 12:18:41 +0100

    Initial commit

---
+++ KramersRateEvaluator.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+
+"""Module for estimating Kramer's hopping rates in an unknown potential energy surface using long-trajectory samples.
+
+   Author: Dominic Phillips (dominicp6)
+"""
+
+import numpy as np
+import warnings
+import pandas as pd
+import pyemma
+
+from autoimpute.imputations import MiceImputer
+from itertools import permutations
+
+from MarkovStateModel import MSM
+import diffusion_utils as utl
+import plot_utils as pltutl
+import general_utils as gutl
+
+
+class KramersRateEvaluator:
+    def __init__(self, verbose=True, default_clustering=None):
+        self.verbose = verbose
+        self.imputer = MiceImputer(strategy={"F": "interpolate"}, n=1, return_list=True)
+        self.default_clustering = default_clustering
+        self.number_of_default_clusters = (
+            None
+            if self.default_clustering is None
+            else len(default_clustering.clustercenters.flatten())
+        )
+
+    def _compute_free_energy(
+        self, time_series, beta, bins=200, impute=True, minimum_counts=25
+    ):
+        counts, coordinates = np.histogram(time_series, bins=bins)
+        coordinates = coordinates[:-1]
+        with np.errstate(divide="ignore"):
+            normalised_counts = counts / np.sum(counts)
+            free_energy = (1 / beta) * gutl.replace_inf_with_nan(
+                -np.log(normalised_counts)
+            )
+
+        if np.isnan(free_energy).any() and impute:
+            # If NaNs found, impute
+            warnings.warn(
+                f"NaN values were found in the free energy calculation. "
+                f"Consider using a longer trajectory or rerunning "
+                f"with fewer bins (currently bins={bins}). Fixing with imputation for now."
+            )
+            print(
+                f"Note: Of the {len(free_energy)} free energy evaluations, "
+                f"{np.count_nonzero(np.isnan(free_energy))} were NaN values."
+            )
+            df = pd.DataFrame({"F": free_energy})
+            free_energy = self.imputer.fit_transform(df)[0][1].F
+        else:
+            # Else compute free energy of bins with counts > minimum_counts
+            robust_counts = counts[np.where(counts > minimum_counts)]
+            normalised_counts = robust_counts / np.sum(counts)
+            free_energy = -(1 / beta) * np.log(normalised_counts)
+            coordinates = coordinates[np.where(counts > minimum_counts)]
+
+        self.coordinates = coordinates
+        self.msd_coordinate = gutl.rms_interval(coordinates)
+        self.free_energy = free_energy - np.min(free_energy)
+
+    def _fit_msm(
+        self, time_series, time_step, lag, cluster_type="kmeans", options=None
+    ):
+
+        if options["dmin"] is None:
+            options["dmin"] = min(
+                10 * self.msd_coordinate,
+                (max(self.coordinates) - min(self.coordinates)) / 10,
+            )
+
+        # 1) Cluster the coordinate space into discrete states
+        if (
+            self.default_clustering is None
+            or self.number_of_default_clusters != options["k"]
+        ):
+            print(
+                f"Debug: default clustering {self.default_clustering}, num default clusters {self.number_of_default_clusters}, k {options['k']}"
+            )
+            cluster = utl.cluster_time_series(time_series, cluster_type, options)
+            self.default_clustering = cluster
+            self.number_of_default_clusters = len(cluster.clustercenters.flatten())
+        else:
+            print(f"Using default clustering provided.")
+            cluster = self.default_clustering
+
+        # 2) Compute the state trajectory
+        discrete_traj = cluster.dtrajs[0].copy()
+        cluster_centers = cluster.clustercenters.copy().flatten()
+        self.msm = MSM(state_centers=cluster_centers)
+        discrete_traj = self.msm.relabel_trajectory_by_coordinate_chronology(
+            traj=discrete_traj
+        )
+
+        if self.verbose:
+            utl.lag_sensitivity_analysis(discrete_traj, cluster_centers, time_step)
+
+        # 3) Use the state trajectory to estimate a best-fitting MSM
+        msm = pyemma.msm.estimate_markov_model(dtrajs=discrete_traj, lag=lag)
+        self.msm.set_stationary_distribution(msm.stationary_distribution)
+        self.msm.set_transition_matrix(msm.transition_matrix)
+        self.msm.set_lag(lag)
+        self.msm.set_time_step(time_step)
+        self.msm.set_discrete_trajectory(discrete_traj)
+        self.diffusion_coefficients = self.msm.compute_diffusion_coefficient(
+            time_step, lag
+        )
+        self.msm.plot()
+
+        return cluster
+
+    def _compute_kramers_rates(
+        self,
+        beta: float,
+        prominence: float,
+        endpoint_minima: bool,
+        high_energy_minima: bool,
+    ):
+        # 1) Compute and plot minima of the free energy landscape
+        free_energy_minima = pltutl.get_minima(
+            self.smoothed_F, prominence, endpoint_minima, high_energy_minima
+        )
+        self.free_energy_minima = [
+            (self.coordinates[minima], self.smoothed_F[minima])
+            for minima in free_energy_minima
+        ]
+        pltutl.plot_free_energy_landscape(self)
+
+        well_integrand = utl.compute_well_integrand(self.smoothed_F, beta)
+        barrier_integrand = utl.compute_barrier_integrand(self, self.smoothed_F, beta)
+
+        # 2) Compute the Kramer's transition rates between every possible pair of minima
+        kramers_rates = []
+        possible_transitions = permutations(range(len(free_energy_minima)), 2)
+        for transition in possible_transitions:
+            kramers_rate = utl.compute_kramers_rate(
+                transition,
+                free_energy_minima,
+                well_integrand,
+                barrier_integrand,
+                self.coordinates,
+            )
+            kramers_rates.append((transition, kramers_rate))
+
+        if self.verbose is True:
+            pltutl.display_kramers_rates(kramers_rates)
+
+        return kramers_rates
+
+    def fit(
+        self,
+        trajectory,
+        beta,
+        time_step,
+        lag,
+        sigmaD,
+        sigmaF,
+        minimum_counts=25,
+        bins=200,
+        impute_free_energy_nans=True,
+        cluster_type="kmeans",
+        k=10,
+        ignore_high_energy_minima=False,
+        include_endpoint_minima=True,
+        minima_prominence=1.5,
+        options=None,
+    ):
+
+        if options is None:
+            options = {
+                "k": k,
+                "stride": 5,
+                "max_iter": 150,
+                "max_centers": 1000,
+                "metric": "euclidean",
+                "n_jobs": None,
+                "dmin": None,
+            }
+
+        self._compute_free_energy(
+            time_series=trajectory,
+            beta=beta,
+            bins=bins,
+            impute=impute_free_energy_nans,
+            minimum_counts=minimum_counts,
+        )
+        self._fit_msm(
+            time_series=trajectory,
+            lag=lag,
+            time_step=time_step,
+            cluster_type=cluster_type,
+            options=options,
+        )
+        self.smoothed_D_domain, self.smoothed_D = gutl.gaussian_smooth(
+            x=self.msm.sorted_state_centers,
+            y=self.diffusion_coefficients,
+            dx=self.msd_coordinate,
+            sigma=sigmaD,
+        )
+        self.smoothed_F_domain, self.smoothed_F = gutl.gaussian_smooth(
+            x=self.coordinates, y=self.free_energy, dx=self.msd_coordinate, sigma=sigmaF
+        )
+        kramers_rates = self._compute_kramers_rates(
+            beta, minima_prominence, include_endpoint_minima, ignore_high_energy_minima
+        )
+
+        return kramers_rates
---
+++ plot_utils.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python
+
+"""General utilities to help with plotting.
+
+   Author: Dominic Phillips (dominicp6)
+"""
+
+import warnings
+from math import ceil, floor
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import numpy as np
+from scipy.signal import find_peaks
+
+from diffusion_utils import (
+    free_energy_estimate,
+    project_points_to_line,
+    free_energy_estimate_2D,
+)
+
+
+def get_digit_text_width(fig, axis):
+    r = fig.canvas.get_renderer()
+    t = axis.text(0.5, 0.5, "1")
+
+    bb = t.get_window_extent(renderer=r).transformed(axis.transData.inverted())
+
+    t.remove()
+
+    return bb.width / 2
+
+
+def display_kramers_rates(kramers_rates):
+    print("Kramer's Rates")
+    print("-" * 25)
+    for rate in kramers_rates:
+        initial_state = rate[0][0]
+        final_state = rate[0][1]
+        transition_rate = rate[1]
+        print(
+            rf"S{initial_state} --> S{final_state} : {'{:e}'.format(transition_rate)}"
+        )
+    print("-" * 25)
+
+
+def get_minima(
+    data_array, prominence, include_endpoint_minima: bool, ignore_high_minima: bool
+):
+    number_of_minima = 0
+    current_prominence = prominence
+    minima = None
+
+    while number_of_minima < 2:
+        minima = find_peaks(-data_array, prominence=current_prominence)[0]
+        energy = [data_array[index] for index in minima]
+
+        if ignore_high_minima:
+            minima = [
+                point
+                for idx, point in enumerate(minima)
+                if not np.abs(energy[idx])
+                > min(data_array) + 0.8 * (max(data_array) - min(data_array))
+            ]
+
+        number_of_minima = len(minima)
+        current_prominence *= 0.975
+
+    if current_prominence != prominence * 0.975:
+        warnings.warn(
+            f"Automatically reduced prominence from {prominence} "
+            f"to {round(current_prominence / 0.975, 3)} so as to find at least two minima."
+        )
+
+    if include_endpoint_minima:
+        if data_array[0] < data_array[1]:
+            minima = np.insert(minima, 0, 0)
+        if data_array[-1] < data_array[-2]:
+            minima = np.append(minima, len(data_array) - 1)
+
+    return minima
+
+
+def plot_minima(minima_list, y_variable):
+    for idx, minima in enumerate(minima_list):
+        print("Minima ", (round(minima[0], 3), round(minima[1], 3)))
+        plt.text(
+            minima[0],
+            minima[1] - 0.075 * (max(y_variable) - min(y_variable)),
+            f"S{idx}",
+            fontsize=16,
+            color="b",
+        )
+
+
+def display_state_boundaries(msm, y_coordinate):
+    voronoi_cell_boundaries = [
+        (msm.sorted_state_centers[i + 1] + msm.sorted_state_centers[i]) / 2
+        for i in range(len(msm.sorted_state_centers) - 1)
+    ]
+    for boundary in voronoi_cell_boundaries:
+        plt.vlines(
+            boundary,
+            ymin=min(y_coordinate) - 0.2 * (max(y_coordinate) - min(y_coordinate)),
+            ymax=min(y_coordinate) - 0.1 * (max(y_coordinate) - min(y_coordinate)),
+            linestyle="--",
+            color="k",
+        )
+
+    return voronoi_cell_boundaries
+
+
+def display_state_numbers(boundaries, x_variable, y_variable, digit_width):
+    x_min = min(x_variable)
+    y_min = min(y_variable)
+    y_range = max(y_variable) - min(y_variable)
+
+    def index_label_width(x):
+        return digit_width * ceil(np.log10(x + 1))
+
+    for state_index in range(len(boundaries) + 1):
+        if state_index == 0:
+            plt.text(
+                (boundaries[state_index] + x_min) / 2 - index_label_width(state_index),
+                y_min - 0.175 * y_range,
+                f"{state_index}",
+                fontsize=12,
+                color="k",
+            )
+        elif state_index == len(boundaries):
+            plt.text(
+                (x_min + boundaries[state_index - 1]) / 2
+                - index_label_width(state_index),
+                y_min - 0.175 * y_range,
+                f"{state_index}",
+                fontsize=12,
+                color="k",
+            )
+        else:
+            plt.text(
+                (boundaries[state_index] + boundaries[state_index - 1]) / 2
+                - index_label_width(state_index),
+                y_min - 0.175 * y_range,
+                f"{state_index}",
+                fontsize=12,
+                color="k",
+            )
+
+
+def plot_free_energy_landscape(self):
+    fig = plt.figure(figsize=(15, 7))
+    ax1 = plt.subplot()
+
+    # Diffusion curve
+    (l1,) = ax1.plot(self.smoothed_D_domain, self.smoothed_D, color="red")
+    ax1.set_ylim(
+        (
+            min(self.smoothed_D) - 0.2 * (max(self.smoothed_D) - min(self.smoothed_D)),
+            max(self.smoothed_D) + 0.1 * (max(self.smoothed_D) - min(self.smoothed_D)),
+        )
+    )
+
+    # Free energy curve
+    ax2 = ax1.twinx()
+    (l2,) = ax2.plot(self.coordinates, self.smoothed_F, color="k")
+    digit_width = get_digit_text_width(fig, ax2)
+    plt.legend([l1, l2], ["diffusion_coefficient", "free_energy"])
+
+    print(f"Free energy profile suggests {len(self.free_energy_minima)} minima.")
+    plot_minima(self.free_energy_minima, self.smoothed_F)
+    state_boundaries = display_state_boundaries(self.msm, self.smoothed_F)
+    if len(state_boundaries) < 50:
+        display_state_numbers(
+            state_boundaries, self.coordinates, self.smoothed_F, digit_width
+        )
+
+    ax1.set_xlabel("Q", fontsize=16)
+    ax1.set_ylabel(r"Diffusion Coefficient $D^{(2)}(Q)$ ($Q^2 / s$)", fontsize=18)
+    ax2.set_ylabel(r"$\mathcal{F}$ ($kJ/mol$)", fontsize=18)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    # plt.title('Free Energy Landscape', fontsize=16)
+    plt.savefig("free_energy_landscape.pdf")
+    plt.show()
+
+
+def plot_free_energy_estimate(potential, samples, beta, name, minimum_counts=50):
+    estimated_free_energy, coordinates = free_energy_estimate(
+        samples, beta, minimum_counts
+    )
+    linear_shift = estimated_free_energy[
+        floor(len(estimated_free_energy) / 2)
+    ] - potential(0)
+
+    fig = plt.figure(figsize=(6, 6))
+    plt.plot(coordinates, estimated_free_energy - linear_shift, "k", label="estimated")
+    plt.xlabel("Q", fontsize=18)
+    plt.ylabel(r"$\mathcal{F}$", fontsize=18)
+    x_range = np.arange(
+        min(coordinates), max(coordinates), (max(coordinates) - min(coordinates)) / 1000
+    )
+    plt.plot(x_range, potential(x_range), label="actual")
+    plt.legend()
+    # plt.title('Free Energy Surface', fontsize=16)
+    plt.savefig(name)
+
+
+def plot_free_energy_slice(samples, beta, slice_centre, slice_angle, minimum_counts=50):
+    concatenated_samples = np.concatenate(samples)
+    projected_samples = project_points_to_line(
+        concatenated_samples, np.array(slice_centre), slice_angle
+    )
+    free_energy, coordinates = free_energy_estimate(
+        projected_samples, beta, minimum_counts
+    )
+    fig, axs = plt.subplots(1, 3)
+    fig.set_size_inches(18, 5)
+    axs[0].hist2d(concatenated_samples[:, 0], concatenated_samples[:, 1], bins=300)
+    axs[0].plot(slice_centre[0], slice_centre[1], "rx", markersize=12)
+    max_x = max(concatenated_samples[:, 0])
+    min_x = min(concatenated_samples[:, 0])
+    x_range = np.arange(min_x, max_x, (max_x - min_x) / 1000)
+    m = np.tan(slice_angle)
+    c = slice_centre[1] - m * slice_centre[0]
+    y_range = m * x_range + c
+    axs[0].plot(x_range, y_range, "r")
+    axs[1].hist(projected_samples, bins=100)
+    axs[2].plot(coordinates, free_energy)
+    plt.show()
+
+    return projected_samples
+
+
+def plot_free_energy_surface(samples, beta, bins=300):
+    concatenated_samples = np.concatenate(samples)
+    free_energy, fig, axs, xedges, yedges = free_energy_estimate_2D(
+        samples, beta, bins=bins
+    )
+    fig.set_size_inches(9, 7)
+    clb = axs.contourf(xedges[1:], yedges[1:], free_energy.T)
+    plt.colorbar(clb, label="Free Energy", ax=axs)
+    plt.xlabel("x", fontsize=16)
+    plt.ylabel("y", fontsize=16)
+    plt.show()
+
+
+def hamiltonian(Q, P, M, U):
+    return -((P**2) / (2 * M) + U(Q))
+
+
+def phase_plot(Q, P, U, M):
+    min_Q = min(Q)
+    max_Q = max(Q)
+    range_Q = max_Q - min_Q
+    min_P = min(P)
+    max_P = max(P)
+    range_P = max_P - min_P
+    Q_range = np.linspace(min_Q - 0.05 * range_Q, max_Q + 0.05 * range_Q, 100)
+    P_range = np.linspace(min_P - 0.05 * range_P, max_P + 0.05 * range_P, 100)
+    Q_mesh, P_mesh = np.meshgrid(Q_range, P_range)
+    plt.pcolormesh(Q_range, P_range, hamiltonian(Q_mesh, P_mesh, M, U))
+    plt.contour(Q_range, P_range, hamiltonian(Q_mesh, P_mesh), levels=15)
+    plt.xlabel(r"$Q$", fontsize=20)
+    plt.ylabel(r"$P$", fontsize=20)
+    plt.plot(Q, P, "k", linewidth=0.7)
+    plt.show()
+
+
+def trajectory_plot(Q0, Q1, U):
+    min_Q0 = min(Q0)
+    max_Q0 = max(Q0)
+    range_Q0 = max_Q0 - min_Q0
+    min_Q1 = min(Q1)
+    max_Q1 = max(Q1)
+    range_Q1 = max_Q1 - min_Q1
+    Q0_range = np.linspace(min_Q0 - 0.05 * range_Q0, max_Q0 + 0.05 * range_Q0, 100)
+    Q1_range = np.linspace(min_Q1 - 0.05 * range_Q1, max_Q1 + 0.05 * range_Q1, 100)
+    Q0_mesh, Q1_mesh = np.meshgrid(Q0_range, Q1_range)
+    plt.pcolormesh(Q0_range, Q1_range, U([Q0_mesh, Q1_mesh]))
+    plt.contour(Q0_range, Q1_range, U([Q0_mesh, Q1_mesh]), levels=15)
+    plt.xlabel(r"$Q_0$", fontsize=20)
+    plt.ylabel(r"$Q_1$", fontsize=20)
+    plt.plot(Q0, Q1, "k", linewidth=0.7)
+    plt.show()
+
+
+def potential_contour_plot(
+    x_min,
+    x_max,
+    y_min,
+    y_max,
+    U,
+    vmin=None,
+    vmax=None,
+    set_min_to_zero=True,
+    save=True,
+    save_name="test",
+):
+    fig, axs = plt.subplots(1, 1)
+    fig.set_size_inches(8, 8)
+    axs.set_aspect("equal")
+    x_range = np.linspace(x_min, x_max, 120)
+    y_range = np.linspace(y_min, y_max, 120)
+    x_mesh, y_mesh = np.meshgrid(x_range, y_range)
+    if set_min_to_zero:
+        U_min = np.min(U([x_mesh, y_mesh]))
+    else:
+        U_min = 0
+    im = axs.pcolormesh(x_range, y_range, U([x_mesh, y_mesh]) - U_min)
+    # axs.contour(x_range, y_range, U([x_mesh, y_mesh]), levels=15)
+    divider = make_axes_locatable(axs)
+    cax = divider.append_axes("right", size="5%", pad=0.20)
+    cb = plt.colorbar(im, cax=cax)
+    cb.set_label(label="Free Energy", fontsize=20)
+    axs.xaxis.set_ticks([-2, -1, 0, 1, 2])
+    axs.yaxis.set_ticks([-2, -1, 0, 1, 2])
+    axs.tick_params(axis="x", labelsize=16)
+    axs.tick_params(axis="y", labelsize=16)
+    if vmin is not None and vmax is not None:
+        im.set_clim(vmin, vmax)
+
+    if save:
+        plt.savefig(save_name, format="pdf", bbox_inches="tight")
+
+    plt.show()
