c7eb2fc1d4caa068d37b452f0a490be4649eece6
domph <dom.phillips97@gmail.com>
2022-11-17 19:38:59 +0000

    Added trajectory_utils.py for traj post-processing

---
+++ utils/trajectory_utils.py
@@ -0,0 +1,23 @@
+import os
+from typing import Optional
+
+import mdtraj as md
+
+
+def clean_and_align_trajectory(working_dir: str, top_name: str, traj_name: str, save_name: Optional[str] = None, remove_water: bool = True, align_protein: bool = True, stride: int = 1):
+    mtraj = md.load_dcd(os.path.join(working_dir, traj_name), top=os.path.join(working_dir, top_name), stride=stride)
+
+    if remove_water:
+        ## Remove waters, ions, and lipid.
+        print('Removing water')
+        mtraj = mtraj.atom_slice(mtraj.top.select('not resname HOH POPC CL NA'))
+
+    if align_protein:
+        ## Centre and align protein; useful for removing artifacts from PBCs
+        print('Aligning protein')
+        prot = mtraj.top.select('protein')
+        mtraj.superpose(mtraj, atom_indices=prot)
+
+    if save_name:
+        print('Saving modified trajectory')
+        mtraj.save(os.path.join(working_dir, f'{save_name}.dcd'))
