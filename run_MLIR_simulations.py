1d61830fd8c16bdca2a493795d6d1cb296bae151
dominicp6 <dom.phillips97@gmail.com>
2023-03-03 12:28:55 +0000

    added LangevinBAOAB

--- run_MLIR_simulations.py
+++ run_MLIR_simulations.py
@@ -1,61 +1,60 @@
-import sys
-sys.path.append('/home/dominic/PycharmProjects/CV_learning')
-from OpenMMSimulation import PDBSimulation, MOL2Simulation
+import argparse
 
-# simulation_params = {
-#     'forcefield': 'amber14',
-#     'precision': 'mixed',
-#     'pdb': '/home/dominic/PycharmProjects/MLIRspec/pdb/alanine.pdb',
-#     'resume': None,
-#     'PLUMED': None,
-#     'gpu': 0,
-#     'duration': '50ns',
-#     'savefreq': '0.1ps',
-#     'stepsize': '1fs',
-#     'temperature': '300K',
-#     'pressure': '1bar',
-#     'frictioncoeff':  '1ps',
-#     'solventpadding': '1nm',
-#     'nonbondedcutoff': '0.7nm',
-#     'cutoffmethod': 'CutoffPeriodic',
-#     'periodic': True,
-#     'minimise': True,
-#     'watermodel': None,
-#     'seed': None,
-#     'name': 'test',
-#     'directory': '/home/dominic/PycharmProjects/MLIRspec/exp',
-#     'equilibrate':  'NPT',
-# }
+from OpenMMSimulation import PDBSimulation
 
 simulation_params = {
     'forcefield': 'amber14',
     'precision': 'mixed',
-    'mol2': '/home/dominic/PycharmProjects/MLIRspec/chemicals/ethene/ethene.mol2',
-    'xml': '/home/dominic/PycharmProjects/MLIRspec/chemicals/ethene/ethene.xml',
     'resume': None,
-    'plumed': None,
+    'PLUMED': None,
     'gpu': 0,
-    'duration': '10ns',
-    'savefreq': '0.1fs',
-    'stepsize': '0.1fs',   # need small stepsize: typical vibrational periods of a molecular bond is 5-20fs
-    'temperature': '300K',
-    'pressure': '',
+    'duration': '50ns',
+    'savefreq': '5ps',
+    'stepsize': '2fs',
+    'temperature': '298K',
+    'pressure': '1bar',
     'frictioncoeff':  '1ps',
     'solventpadding': '1nm',
     'nonbondedcutoff': '0.7nm',
-    'cutoffmethod': 'NoCutoff',
+    'cutoffmethod': 'CutoffPeriodic',
     'periodic': True,
     'minimise': True,
     'watermodel': None,
     'seed': None,
     'name': 'test',
-    'directory': '/home/dominic/PycharmProjects/MLIRspec/exp',
-    'equilibrate':  'NVT',
-    'integrator': 'Langevin',
+    'directory': '/home/dominic/PycharmProjects/CV_learning/MLIRexp',
+    'equilibrate':  'NPT',
+    'integrator': 'LangevinBAOAB',
 }
 
 if __name__ == '__main__':
-    simulation = MOL2Simulation().from_args(args=simulation_params)
-    # print(simulation.generate_executable_command(args=simulation_params))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('system', type=str, default=None, help='System to simulation '
+                                                               '(Options: BCT, COT, DMF, GDP, GTP, H2P, HPO, NML)')
+    args = parser.parse_args()
+
+    simulation_params['pdb'] = f'./chemicals/{args.system}.pdb'
+    simulation = PDBSimulation().from_args(args=simulation_params)
     simulation.run()
-    
+
+"""
+BCT Error:
+    system = self.force_field.createSystem(
+  File "/home/dominic/miniconda3/envs/diffusion/lib/python3.9/site-packages/openmm/app/forcefield.py", line 1212, in createSystem
+    templateForResidue = self._matchAllResiduesToTemplates(data, topology, residueTemplates, ignoreExternalBonds)
+  File "/home/dominic/miniconda3/envs/diffusion/lib/python3.9/site-packages/openmm/app/forcefield.py", line 1427, in _matchAllResiduesToTemplates
+    raise ValueError('No template found for residue %d (%s).  %s' % (res.index+1, res.name, _findMatchErrors(self, res)))
+ValueError: No template found for residue 1 (BCT).  The set of atoms is similar to ASP, but it is missing 7 atoms.
+
+COT Error:
+    system = self.force_field.createSystem(
+  File "/home/dominic/miniconda3/envs/diffusion/lib/python3.9/site-packages/openmm/app/forcefield.py", line 1212, in createSystem
+    templateForResidue = self._matchAllResiduesToTemplates(data, topology, residueTemplates, ignoreExternalBonds)
+  File "/home/dominic/miniconda3/envs/diffusion/lib/python3.9/site-packages/openmm/app/forcefield.py", line 1427, in _matchAllResiduesToTemplates
+    raise ValueError('No template found for residue %d (%s).  %s' % (res.index+1, res.name, _findMatchErrors(self, res)))
+ValueError: No template found for residue 1 (COT).  This might mean your input topology is missing some atoms or bonds, or possibly that you are using the wrong force field.
+
+
+
+"""
